66bbd9090f934774701cfcfb72dcb7320efd81ae
MNafekh <martinnafekh@hotmail.com>
2015-05-20 16:12:46 -0400

    Started table creation statements

--- SQL/createDB.sql
+++ SQL/createDB.sql
@@ -1,3 +1,6 @@
 CREATE DATABASE onsale.gg;
 USE onsale.gg;
 
+CREATE TABLE skins (name VARCHAR(50), cost INTEGER, released DATE, last_on_sale DATE);
+
+CREATE TABLE pricing (cost INTEGER, tier VARCHAR(20));
