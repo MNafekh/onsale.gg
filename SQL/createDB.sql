9ac8f3345da2e71ff5b82c3524e52308142a4c29
MNafekh <martinnafekh@hotmail.com>
2015-05-23 15:08:41 -0400

    Tentative finished table creation statements

--- SQL/createDB.sql
+++ SQL/createDB.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE onsale.gg;
 USE onsale.gg;
 
-CREATE TABLE skins (name VARCHAR(50), cost INTEGER, released DATE, last_on_sale DATE);
+CREATE TABLE skins (ID int NOT NULL AUTO_INCREMENT, skin_name VARCHAR(50), tier VARCHAR(20), 
+	sale_price DOUBLE, released DATE, last_on_sale DATE, PRIMARY KEY (ID), 
+	FOREIGN KEY (tier) REFERENCES pricing(tier));
 
-CREATE TABLE pricing (cost INTEGER, tier VARCHAR(20));
+CREATE TABLE pricing (cost INTEGER, tier VARCHAR(20), PRIMARY KEY (tier));
